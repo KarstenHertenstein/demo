b8d5c6749fd081efc4e4d6ca6e6d5ba1bf47ca22
KarstenHertenstein <karstenhertenstein@mail.de>
2020-08-13 04:36:51 +0200

    Implemented 8x8 Gridlayout GUI

---
+++ src/main/java/GUI.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeListener;
+
+public class GUI{
+
+    private final JPanel gui = new JPanel(new BorderLayout(3, 3));
+    private JButton[][] chessBoardSquares = new JButton[8][8];
+    private JPanel chessBoard;
+
+    GUI() {
+        initializeGui();
+    }
+
+    public final void initializeGui() {
+        // set up the main GUI
+        gui.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        chessBoard = new JPanel(new GridLayout(8, 8));
+        chessBoard.setBorder(new LineBorder(Color.BLACK));
+        gui.add(chessBoard);
+
+        // create the chess board squares
+        Insets buttonMargin = new Insets(0,0,0,0);
+        for (int i = 0; i < chessBoardSquares.length; i++) {
+            for (int j = 0; j < chessBoardSquares[i].length; j++) {
+                JButton b = new JButton();
+                b.setMargin(buttonMargin);
+                // our chess pieces are 64x64 px in size, so we'll
+                // 'fill this in' using a transparent icon..
+                ImageIcon icon = new ImageIcon(
+                        new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+                b.setIcon(icon);
+                if ((j % 2 == 1 && i % 2 == 1)
+                        //) {
+                        || (j % 2 == 0 && i % 2 == 0)) {
+                    b.setBackground(Color.WHITE);
+                } else {
+                    b.setBackground(Color.BLACK);
+                }
+                chessBoardSquares[j][i] = b;
+            }
+        }
+
+        // fill the black non-pawn piece row
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                chessBoard.add(chessBoardSquares[j][i]);
+            }
+        }
+
+    }
+
+
+    public final JComponent getChessBoard() {
+        return chessBoard;
+    }
+
+    public final JComponent getGui() {
+        return gui;
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,27 @@
-public class main {
+import javax.swing.*;
+
+public class main{
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Runnable r = new Runnable() {
+
+            @Override
+            public void run() {
+                GUI gui = new GUI();
+
+                JFrame f = new JFrame("Chess");
+                f.add(gui.getGui());
+                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                f.setLocationByPlatform(true);
+
+                // ensures the frame is the minimum size it needs to be
+                // in order display the components within it
+                f.pack();
+                // ensures the minimum size is enforced.
+                f.setMinimumSize(f.getSize());
+                f.setVisible(true);
+            }
+        };
+        SwingUtilities.invokeLater(r);
     }
 }
